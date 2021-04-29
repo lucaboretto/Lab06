2285d6cf952ed88f2cc54f43cf8d32fdfc88a38e
Luca Boretto <Luca Boretto@User-PC>
2021-04-29 17:58:29 +0200

    Versione 1

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/MeteoDAO.java
@@ -39,9 +39,70 @@ public class MeteoDAO {
 		}
 	}
 
-	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
+	public String getUmiditaMedie(int mese){
+		
+		String umidita="";
+		
+		final String sql = "SELECT Localita, AVG(Umidita) AS u "
+				+ "FROM situazione "
+				+ "WHERE MONTH(DATA) = ? "
+				+ "GROUP BY Localita";
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
 
-		return null;
+			st.setInt(1, mese);
+			ResultSet rs = st.executeQuery();
+			
+			
+			while (rs.next()) {
+				umidita += rs.getString("Localita");
+				umidita += " " + rs.getString("u") + "\n";
+			}
+
+			conn.close();
+			return umidita;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+		
+	
+	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
+		
+		List<Rilevamento> rilevamentiByLocalitaMese = new ArrayList<>();
+		
+				final String sql = "SELECT Localita, Umidita, Data "
+						+ "FROM situazione "
+						+ "WHERE MONTH(DATA) = ? "
+						+ "AND Localita = ?";
+				
+				try {
+					Connection conn = ConnectDB.getConnection();
+					PreparedStatement st = conn.prepareStatement(sql);
+
+					st.setInt(1, mese);
+					st.setString(2, localita);
+					ResultSet rs = st.executeQuery();
+					
+					
+					while (rs.next()) {
+						Rilevamento r = new Rilevamento(rs.getString("Localita"), rs.getDate("Data"), rs.getInt("Umidita"));
+						rilevamentiByLocalitaMese.add(r);
+					}
+
+					conn.close();
+					return rilevamentiByLocalitaMese;
+
+				} catch (SQLException e) {
+
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
 	}
 
 
--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/model/Model.java
@@ -1,6 +1,9 @@
 package it.polito.tdp.meteo.model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import it.polito.tdp.meteo.DAO.MeteoDAO;
 
 public class Model {
 	
@@ -9,19 +12,107 @@ public class Model {
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
 
+	List <Rilevamento> rilevamenti;
+	MeteoDAO dao;
+	int costoMin;
+	List<Citta> citta = new ArrayList<>();
+	List<Citta> result;
+	
 	public Model() {
-
+		 dao = new MeteoDAO();
+		 rilevamenti = new ArrayList<Rilevamento>(dao.getAllRilevamenti());	
 	}
 
 	// of course you can change the String output with what you think works best
 	public String getUmiditaMedia(int mese) {
-		return "TODO!";
+		String s = dao.getUmiditaMedie(mese);
+		return s;
 	}
 	
 	// of course you can change the String output with what you think works best
-	public String trovaSequenza(int mese) {
-		return "TODO!";
+	public List<Citta> trovaSequenza(int mese) {
+		
+		List<Citta> parziale = new ArrayList<Citta>();
+		costoMin = 999999999;
+		
+		citta.add(new Citta("Genova", dao.getAllRilevamentiLocalitaMese(mese, "Genova")));
+		citta.add(new Citta("Milano", dao.getAllRilevamentiLocalitaMese(mese, "Milano")));
+		citta.add(new Citta("Torino", dao.getAllRilevamentiLocalitaMese(mese, "Torino")));
+	
+		for(Citta c: citta) {
+			c.setRilevamenti(dao.getAllRilevamentiLocalitaMese(mese, c.getNome()));
+		}
+		
+		boolean spostamento = false;
+		int contaGiorniFermo = 0;
+		
+		ricorsione(0, parziale, 0, citta, spostamento, contaGiorniFermo);
+		
+		
+		return result;
 	}
 	
-
+	private boolean controllaCitta() {
+		int cont = 0;
+		for(Citta c: citta) {
+			if(c.getCounter()>0)
+				cont++;
+		}
+		if(cont == citta.size())
+			return true;
+		return false;
+	}
+	
+	private void ricorsione(int livello, List<Citta> parziale, int costo, List<Citta> citta, boolean spostamento, int contaGiorniFermo){
+		
+		if(livello == 15) {
+			if(costo < costoMin && this.controllaCitta()) {
+				costoMin = costo;
+				result = new ArrayList<>(parziale);
+			}
+			return;
+		}else {
+			for(Citta c: citta) {
+				if(c.getCounter()<this.NUMERO_GIORNI_CITTA_MAX) {   //citta ancora disponibile
+					if(contaGiorniFermo < this.NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN) {
+						//ricorsione da fermo
+						costo += c.getRilevamenti().get(livello).getUmidita();
+						contaGiorniFermo++;
+						spostamento = false;
+						parziale.add(c);
+						c.setCounter(c.getCounter()+1);
+						ricorsione(livello+1, parziale, costo, citta, spostamento, contaGiorniFermo);
+					}else {
+						if(parziale.size()>0) {		
+							if(!(parziale.get(parziale.size()-1).equals(c))) {   //spostamento
+								costo += this.COST;
+								spostamento = true;
+								contaGiorniFermo = 0;
+							}else {
+								spostamento = false;
+							}
+							costo += c.getRilevamenti().get(livello).getUmidita();
+							contaGiorniFermo++;
+							parziale.add(c);
+							c.setCounter(c.getCounter()+1);
+							ricorsione(livello+1, parziale, costo, citta, spostamento, contaGiorniFermo);
+					}
+					}
+					//backtracking
+						
+					costo -= c.getRilevamenti().get(livello).getUmidita();	
+					c.setCounter(c.getCounter()-1);
+					parziale.remove(c);
+					
+					contaGiorniFermo--;
+					if(spostamento) {
+						contaGiorniFermo = 0;
+						costo -= this.COST;	
+						spostamento = false;
+					}
+					
+				}			
+     		}
+		}
+	}
 }
